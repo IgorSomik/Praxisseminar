0c608855fdff3a4874176a8a4bcd2958675f3423
Igor S <Igor_Somik@web.de>
2017-12-14 22:14:05 +0100

    APK updated, objects in scene added
    The APK was updated. New objects were added to the scene. Animations
    were added and some minor issues were fixed.

--- AR Praxisseminar/Assets/Scripts/VuforiaNische/UIController.cs
+++ AR Praxisseminar/Assets/Scripts/VuforiaNische/UIController.cs
@@ -9,20 +9,60 @@ public class UIController : MonoBehaviour {
     public GameObject TextCrane;
     public GameObject TextBowl;
     public GameObject TextHandle;
+    public GameObject EwerCap;
+    public GameObject WaterInPod;
+    public GameObject WaterInBowl;
+    public GameObject WaterFlow;
+    public GameObject WaterSurfaceCrane;
 
     public GameObject Vase;
 
     private GameObject[] clickables;
     private bool textCurrentlyShown = false;
+    private bool handleClicked = false;
+    private bool craneClicke = false;
+    private bool bowlClicked = false;
     private GameObject displayedText;
     private GameObject clickedSelectable;
+    private Vector3 startPositionCap;
+    private Vector3 startPositionWaterCrane;
+    private Vector3 startPositionWaterBowl;
 
 
     private void Start()
     {
         clickables = GameObject.FindGameObjectsWithTag("Clickables");
+        startPositionCap = EwerCap.transform.position;
+        startPositionWaterCrane = WaterSurfaceCrane.transform.position;
+        startPositionWaterBowl = WaterInBowl.transform.position;
     }
 
+    private void Update()
+    {
+        if (handleClicked)
+        {
+            if(EwerCap.transform.position.y < 8.5f)
+            {
+                EwerCap.transform.position += new Vector3(0, 0.05f, 0);
+            }
+        }
+
+        if (craneClicke)
+        {
+            if(WaterSurfaceCrane.transform.position.y < 2.25f)
+            {
+                WaterSurfaceCrane.transform.position += new Vector3(0, 0.005f, 0);
+            }
+        }
+
+        if (bowlClicked)
+        {
+            if(WaterInBowl.transform.position.y < 2.6f)
+            {
+                WaterInBowl.transform.position += new Vector3(0, 0.005f, 0);
+            }
+        }
+    }
 
     public void clickOnParticle(GameObject clickedObject)
     {
@@ -38,7 +78,6 @@ public class UIController : MonoBehaviour {
             }
             activateTextForClickedObject(clickedObject.name);
             textCurrentlyShown = true;
-            //Canves.SetActive(true);
         }
         else
         {
@@ -51,16 +90,27 @@ public class UIController : MonoBehaviour {
         if(name == "ParticleCrane")
         {
             displayedText = TextCrane;
+            WaterFlow.SetActive(true);
+            Invoke("showWaterInBowl", 0.7f);
         }
         else if(name == "ParticleBowl")
         {
             displayedText = TextBowl;
+            WaterInBowl.SetActive(true);
+            bowlClicked = true;
         }
         else
         {
             displayedText = TextHandle;
+            handleClicked = true;
+            WaterInPod.SetActive(true);
         }
         displayedText.SetActive(true);
+    }
+
+    private void showWaterInBowl()
+    {
+        craneClicke = true;
     }
 
     public void showAllClickables()
@@ -73,9 +123,32 @@ public class UIController : MonoBehaviour {
             }
         }
 
+        if (WaterFlow.activeInHierarchy)
+        {
+            WaterFlow.SetActive(false);
+        }
+
+        if (handleClicked)
+        {
+            handleClicked = false;
+            EwerCap.transform.position = startPositionCap;
+            WaterInPod.SetActive(false);
+        }
+
+        if (craneClicke)
+        {
+            craneClicke = false;
+            WaterSurfaceCrane.transform.position = startPositionWaterCrane;
+        }
+
+        if (bowlClicked)
+        {
+            WaterInBowl.SetActive(false);
+            bowlClicked = false;
+            WaterInBowl.transform.position = startPositionWaterBowl;
+        }
         clickedSelectable = null;
         textCurrentlyShown = false;
         displayedText.SetActive(false);
-        //Canves.SetActive(false);
     }
 }
