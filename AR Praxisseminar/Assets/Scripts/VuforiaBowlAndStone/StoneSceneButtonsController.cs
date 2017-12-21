3074a6ba35290eb89f62f70a4c897aba58472a25
Igor S <Igor_Somik@web.de>
2017-12-21 01:43:47 +0100

    Added buttons and animation in stone scene

---
+++ AR Praxisseminar/Assets/Scripts/VuforiaBowlAndStone/StoneSceneButtonsController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StoneSceneButtonsController : MonoBehaviour {
+
+    public GameObject LatinTextObject;
+    public GameObject ShowHideTextButton;
+    public GameObject ShowGerOrLatButton;
+
+    private bool textDisplayed = false;
+    private bool gerTextShown = false;
+
+
+    public void showOrHideLatinText()
+    {
+        textDisplayed = !textDisplayed;
+
+        if (textDisplayed)
+        {
+            LatinTextObject.SetActive(true);
+            ShowHideTextButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("hideText");
+        }
+        else
+        {
+            LatinTextObject.SetActive(false);
+            ShowHideTextButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("showText");
+        }
+    }
+
+
+    public void switchTextToGerOrLat()
+    {
+        gerTextShown = !gerTextShown;
+
+        if (gerTextShown)
+        {
+            ShowGerOrLatButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("latText");
+        }
+        else
+        {
+            ShowGerOrLatButton.GetComponent<Image>().sprite = Resources.Load<Sprite>("gerText");
+        }
+    }
+}
